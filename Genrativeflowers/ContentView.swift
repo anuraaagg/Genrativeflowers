9f8b36b5f44820bc40a28b4be2304960a88d9fdd
anuraaagg <s.anurag1998@gmail.com>
2025-12-02 18:29:18 +0530

    Initial Commit

---
+++ Genrativeflowers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Genrativeflowers
+//
+//  Created by Anurag Singh on 02/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Genrativeflowers/GenrativeflowersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenrativeflowersApp.swift
+//  Genrativeflowers
+//
+//  Created by Anurag Singh on 02/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GenrativeflowersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
